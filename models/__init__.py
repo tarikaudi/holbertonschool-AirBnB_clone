3a9ab635c5cfcf7e0b02b103a27824666f21e379
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-07-03 19:21:36 -0300

    Empty Repo

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Create the file storage instance"""
-
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload()
--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-# /usr/bin/python3
-"""Contains the class definition for Amenity"""
-
-from models. base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Contains the class attribute"""
-    name = ""
